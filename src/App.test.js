28abbbf84d2ba543cf0b5fb766bc59eb8c2426ed
Kim Ly Buck <kim.3993@gmail.com>
2020-04-19 14:59:29 -0500

    forked, update package.json so the app can run

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import App from './App';
 
-test("renders App without crashing", () => {
-  render(<App />);
+test.afterEach(cleanup);
+
+test('renders App without crashing', () => {
+	render(<App />);
 });
